fce6106d6c01d6a1f44a04dff50c51de13206e19
zed <zed@qq.com>
2022-08-07 11:30:56 +0800

    thread

--- src/com/atguigu/Hello1.java
+++ src/com/atguigu/Hello1.java
@@ -4,5 +4,6 @@ public class Hello1 {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
