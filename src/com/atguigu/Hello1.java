ab7c54fa9be1dbba007c2e50a45cc74b77102f68
zed <zed@qq.com>
2022-08-08 08:30:47 +0800

    master1

--- src/com/atguigu/Hello1.java
+++ src/com/atguigu/Hello1.java
@@ -6,5 +6,6 @@ public class Hello1 {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hot1");
+        System.out.println("master");
     }
 }
